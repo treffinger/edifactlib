2bc92f42439f5985b66ab64b679acf2a0b0264ee
Matti Treffinger <82829772+treffinger@users.noreply.github.com>
2026-09-23 19:12:21 +0200

    Fix EdifactError string rendering when no error target is set

--- src/edifactlib/core/exceptions/edifact_error.py
+++ src/edifactlib/core/exceptions/edifact_error.py
@@ -10,12 +10,13 @@ class EdifactError(Exception):
         self.details = details if details is not None else ErrorDetails()
 
     def __str__(self) -> str:
-        if not self.details.interchange:
+        target = self.details.component or self.details.data_element or self.details.segment
+
+        if not self.details.interchange or not target:
             return self.message
 
-        target = self.details.component or self.details.data_element or self.details.segment
         interchange_text = self.details.interchange.dump_raw(None, target, self._red)
-        return f"{self.message}\n\nFaulty part: \n\n{interchange_text}"
+        return f"{self.message}\n\nFaulty part:\n{interchange_text}"
 
     def _red(self, text: str) -> str:
         return f"\033[31m{text}\033[0m"
